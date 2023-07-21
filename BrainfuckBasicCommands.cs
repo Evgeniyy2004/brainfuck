0923f9c0a7d6f0876d6a54d84a49cb345dedbb31
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-07-21 12:27:55 +0500

    Добавьте файлы проекта.

---
+++ BrainfuckBasicCommands.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace func.brainfuck
+{
+	public class BrainfuckBasicCommands
+	{
+		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
+		{
+			vm.RegisterCommand('.', b => write(System.Text.Encoding.ASCII.GetString(vm.Memory, vm.MemoryPointer, 1)[0]));
+            vm.RegisterCommand('+', b =>
+            {
+                if (vm.Memory[vm.MemoryPointer]<255) vm.Memory[vm.MemoryPointer]++;
+                else vm.Memory[vm.MemoryPointer] = 0;
+
+            });
+            vm.RegisterCommand('-', b =>
+            {
+                if (vm.Memory[vm.MemoryPointer] > 0) vm.Memory[vm.MemoryPointer]--;
+                else vm.Memory[vm.MemoryPointer] = 255;
+
+            });
+            vm.RegisterCommand(',', b => vm.Memory[vm.MemoryPointer]=(byte)read());
+            vm.RegisterCommand('>', b => 
+            {
+                if (vm.MemoryPointer < vm.Memory.Length - 1) vm.MemoryPointer++;
+                else vm.MemoryPointer=0;
+                    
+            });
+            vm.RegisterCommand('<', b =>
+            {
+                if (vm.MemoryPointer> 0) vm.MemoryPointer--;
+                else vm.MemoryPointer = vm.Memory.Length-1;
+
+            });
+            for (char i='a';i<='z';i++)
+			{
+				var c =i;
+                var l = (byte)i;
+                vm.RegisterCommand(c, b => vm.Memory[vm.MemoryPointer]= l);
+            }
+
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                var c = i;
+                var l = (byte)i;
+                vm.RegisterCommand(c, b => vm.Memory[vm.MemoryPointer] = l);
+            }
+
+            for (int i = 0;i<10;i++)
+            {
+                var c = (char)i;
+                var l = (byte)i;
+                vm.RegisterCommand(c, b => vm.Memory[vm.MemoryPointer] = l);
+            }
+        }
+	}
+}
---
+++ VirtualMachine.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace func.brainfuck
+{
+	public class VirtualMachine : IVirtualMachine
+	{
+		public string Instructions { get; }
+		public int InstructionPointer { get; set; }
+		public byte[] Memory { get; }
+		public int MemoryPointer { get; set; }
+		Dictionary<char,Action<IVirtualMachine>> all =new Dictionary<char, Action<IVirtualMachine>>();
+
+		public VirtualMachine(string program, int memorySize)
+		{
+			Memory=new byte[memorySize];
+			Instructions=program;
+			InstructionPointer = 0;
+			MemoryPointer = 0;
+		}
+
+		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
+		{
+			if(!all.ContainsKey(symbol)) all.Add(symbol, execute);
+		}
+
+		public void Run()
+		{
+			while (InstructionPointer < Instructions.Length)
+			{
+				if (all.ContainsKey(Instructions[InstructionPointer]))
+				{
+					all[Instructions[InstructionPointer]](this);
+				}
+                InstructionPointer++;
+            }
+		}
+	}
+}
